dc75efd20edf0a623e8eb4347f971295c67239fe
shyamr586 <v.shyamr586@gmail.com>
2020-11-06 15:45:41 +0400

    changed layout, bg, js file

---
+++ classlist.js
@@ -0,0 +1,19 @@
+let classlist = [
+
+    {
+        "subject": "Mathematics",
+        "location": "London",
+        "price": "$200",
+        "spaces": "5",
+        "image": "https://thumbs.dreamstime.com/b/mathematics-text-back-to-school-wallpaper-education-background-concept-stationery-banner-colorful-pencil-d-rendering-157634684.jpg",
+    },
+
+    {
+        "subject": "Physics",
+        "location": "London",
+        "price": "$300",
+        "spaces": "10",
+        "image": "https://www.environmentalscience.org/wp-content/uploads/2018/08/physics-640x416.jpg",
+    },
+
+]
